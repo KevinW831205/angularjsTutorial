e495c6bd682d2f6ba4985e33042cd1ec01dd4a1e
KevinW <bird831205@hotmail.com>
2020-06-07 19:23:39 -0400

    DI in angular js

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,24 +1,29 @@
 angular.
   module('phonecatApp').
   component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
-    templateUrl:'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.',
-          age: 1
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 2
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 3
-        }
-      ];
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: function PhoneListController($http) {
+      // this.phones = [
+      //   {
+      //     name: 'Nexus S',
+      //     snippet: 'Fast just got faster with Nexus S.',
+      //     age: 1
+      //   }, {
+      //     name: 'Motorola XOOM™ with Wi-Fi',
+      //     snippet: 'The Next, Next Generation tablet.',
+      //     age: 2
+      //   }, {
+      //     name: 'MOTOROLA XOOM™',
+      //     snippet: 'The Next, Next Generation tablet.',
+      //     age: 3
+      //   }
+      // ];
+      let self = this;
+      self.orderProp = 'age';
 
-      this.orderProp = 'age';
+      $http.get('phones/phones.json').then(function (response) {
+        self.phones = response.data
+      })
+
     }
   });
